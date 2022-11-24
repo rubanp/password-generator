cb2c1d64bc864baf27e152511bc31df4322e91d6
Ruban Prakash <rubanprakash@hotmail.com>
2022-11-24 13:06:08 +0000

    change font weight for text styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,24 @@
-<script setup lang="ts">
+<script setup>
+  const password = ref('')
 </script>
 
 <template>
-  <div class="main-container">
+  <div class="index-container">
     <h1 class="heading-medium">Password Generator</h1>
-    <PasswordDisplay password="!Lmv;67m.565"/>
-    <PasswordGenerator />
+    <PasswordDisplay :password="password"/>
+    <PasswordGenerator @password="(newPassword) => password = newPassword"/>
   </div>
 </template>
 
 <style scoped>
-.main-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2em;
-}
+  .index-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2em;
+  }
 
-h1 {
-  color: var(--light-grey);
-}
+  h1 {
+    color: var(--light-grey);
+  }
 </style>
