1092071d335ff85b02dd81fc96831d2f88fd7ea4
Ruban Prakash <rubanprakash@hotmail.com>
2022-11-29 17:06:49 +0000

    add password strength story

---
+++ components/Stories/PasswordStrength.story.vue
@@ -0,0 +1,19 @@
+<template>
+  <Story title="Password Strength">
+    <Variant title="Default">
+      <PasswordStrength></PasswordStrength>
+    </Variant>
+    <Variant title="Too Weak">
+      <PasswordStrength strength="1"></PasswordStrength>
+    </Variant>
+    <Variant title="Weak">
+      <PasswordStrength strength="2"></PasswordStrength>
+    </Variant>
+    <Variant title="Medium">
+      <PasswordStrength strength="3"></PasswordStrength>
+    </Variant>
+    <Variant title="Strong">
+      <PasswordStrength strength="4"></PasswordStrength>
+    </Variant>
+  </Story>
+</template>
